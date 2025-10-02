c5a0139a70511ec389f88202738b87fb1404c8aa
Hemanth T <hemanth.t@expeditors.com>
2025-10-02 23:45:47 +0530

    checking authentication

--- src/linkedList/addTwoNumbersII/Solution.java
+++ src/linkedList/addTwoNumbersII/Solution.java
@@ -1,15 +1,12 @@
 package linkedList.addTwoNumbersII;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode first = reverse(l1);
